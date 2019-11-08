aff35b9767ffe2976782a7e13fe95d59ea24b965
Ted <42992766+ted-lendit@users.noreply.github.com>
2019-11-08 12:16:43 +0900

    channel-example 주석 추가 (#3)
    * 스터디 주석 추가

--- examples/channel/channel-example-1.kt
+++ examples/channel/channel-example-1.kt
@@ -4,7 +4,10 @@ import channel.*
 import delay.*
 
 // https://tour.golang.org/concurrency/1
-
+/**
+ * Suspend 키워드는 함수를 suspend 하게 실행시킬 수 있게 하고
+ * corountine context 안에서만 실행할 수 있다.
+ */
 suspend fun say(s: String) {
     for (i in 0..4) {
         delay(100)
@@ -12,6 +15,9 @@ suspend fun say(s: String) {
     }
 }
 
+/**
+ *
+ */
 fun main(args: Array<String>) = mainBlocking {
     go { say("world") }
     say("hello")
--- examples/channel/channel-example-4.kt
+++ examples/channel/channel-example-4.kt
@@ -1,6 +1,7 @@
 package channel.example4
 
 import channel.*
+import delay.delay
 
 // https://tour.golang.org/concurrency/4
 
@@ -9,6 +10,7 @@ suspend fun fibonacci(n: Int, c: SendChannel<Int>) {
     var y = 1
     for (i in 0..n - 1) {
         c.send(x)
+        println("send #$i: $x")
         val next = x + y
         x = y
         y = next
@@ -16,10 +18,18 @@ suspend fun fibonacci(n: Int, c: SendChannel<Int>) {
     c.close()
 }
 
+/**
+ * capaity 는 피보나치 알고리즘에 영향은 전혀 없음
+ */
 fun main(args: Array<String>) = mainBlocking {
     val c = Channel<Int>(2)
     go { fibonacci(10, c) }
+
+    var cnt = 0
+    // channel 에 수신되서 iterable 할 값이 있을 때 마다 for 문 안에가 실행된다.
+    // capaicty 값을 조정해가며 실행해보자.
     for (i in c) {
-        println(i)
+        delay(3000)
+        println("receive #${cnt++}: $i")
     }
 }
